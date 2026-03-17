351cf04eeb0f3540b135f199d78825f91d85a7e1
Sarveshwar Kumar Shukla <sarveshwarkumarshukla@gmail.com>
2026-03-17 12:08:32 +0530

    remove some checks

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "https://todofrontend-dg3x.onrender.com",
-    "http://localhost:5173",
+    // "http://localhost:5173",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
